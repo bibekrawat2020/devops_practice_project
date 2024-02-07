de54e781369dba827bd7272dcb4eadd4e1a497dd
Ubuntu <ubuntu@ip-172-31-81-148.ec2.internal>
2024-02-07 12:08:12 +0000

    update

--- all_in_docker/server/index.js
+++ all_in_docker/server/index.js
@@ -9,7 +9,7 @@ app.use(cors({
 
 const db = mysql.createPool({
   port: 3306,
-  host: "1e1f51e514ab",
+  host: "08bbb198ca30",
   user: "root",
   password: "mauFJcuf5dhRMQrjj",
   database: "employeeSystem",
